0851ac58abb0fb1a6b036c38c07c0f171aa230ac
Felipe Ramirez <felipe.ramirez@bridge-u.com>
2022-08-13 16:11:45 +0200

    Added Profile and Stats endpoints

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+import requests as r
 
+#Creating the flask app
 app = Flask(__name__)
 
+#Creating an API Object
+api = Api(app)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+TRN_API_KEY = 'fd75b598-5e7f-4de9-a6ba-f3e39f918876'
+class Home(Resource):
+
+    def get(self):
+        return jsonify({'message': 'Hello World!'})
+
+class Profile(Resource):
+
+    def get(self, profile_user_identifier):
+        PROFILE_URL='https://public-api.tracker.gg/v2/csgo/standard/profile/steam/' + profile_user_identifier
+        params = {'TRN-Api-Key': TRN_API_KEY}
+        data = r.get(url = PROFILE_URL, params = params).json()
+        return jsonify({'data': data})
+
+class Stats(Resource):
+    def get(self, profile_user_identifier, segment_type):
+        PROFILE_URL='https://public-api.tracker.gg/v2/csgo/standard/profile/steam/' + profile_user_identifier + '/segments/' + segment_type
+        params = {'TRN-Api-Key': TRN_API_KEY}
+        data = r.get(url = PROFILE_URL, params = params).json()
+        return jsonify({'data': data})
+
+
+#adding the defined resources along with their corresponding urls
+api.add_resource(Home, '/')
+api.add_resource(Profile, '/profile/<string:profile_user_identifier>')
+api.add_resource(Stats, '/stats/<string:profile_user_identifier>/<string:segment_type>')
 
 
 if __name__ == '__main__':
