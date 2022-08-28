8d3cfc049738f8fe034458afb0c9f5c48ee713c7
Felipe Ramirez <feliperamigon1704@gmail.com>
2022-08-28 18:33:42 +0200

    Fix profile resource

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,3 @@
 TRN_API_KEY = 'fd75b598-5e7f-4de9-a6ba-f3e39f918876'
 PUBLIC_TRN_URL = 'https://public-api.tracker.gg/v2/csgo/standard/profile/steam/'
+STEAM_WEB_API_KEY = 'CC69318127F1641933C25DF37EC05373'
